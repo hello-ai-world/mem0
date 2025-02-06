75a438bfc583ebe0f93127f564284ce9b2fefdfd
suchuanjie <suchuanjie@lifepilot.xyz>
2025-02-06 15:49:30 +0800

    change for only be a submoudle

--- mem0/__init__.py
+++ mem0/__init__.py
@@ -1,6 +1,11 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version("mem0ai")
+#__version__ = importlib.metadata.version("mem0ai")
+__version__ = '1.0.49'
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
 from mem0.client.main import MemoryClient, AsyncMemoryClient  # noqa
 from mem0.memory.main import Memory  # noqa
